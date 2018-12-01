ee9b00bc494ba270306e32fa373415bf56a108c3
Hendi Santika <hendisantika@gmail.com>
2018-12-02 06:11:17 +0700

    #4 - Add Person Repository

---
+++ src/main/java/com/hendisantika/springredisexample/repository/PersonRepository.java
@@ -0,0 +1,56 @@
+package com.hendisantika.springredisexample.repository;
+
+import com.hendisantika.springredisexample.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-redis-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-12-02
+ * Time: 06:10
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+@Transactional
+public class PersonRepository {
+    private static final String KEY = "persons";
+    @Autowired
+    private RedisTemplate<String, Person> template;
+
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, Integer, Person> hashOps;
+
+    public void addPerson(Person person) {
+        hashOps.putIfAbsent(KEY, person.getId(), person);
+    }
+
+    public void updatePerson(Person person) {
+        hashOps.put(KEY, person.getId(), person);
+    }
+
+    public Person getPerson(Integer id) {
+        return hashOps.get(KEY, id);
+    }
+
+    public long getNumberOfPersons() {
+        return hashOps.size(KEY);
+    }
+
+    public Map<Integer, Person> getAllPersons() {
+        return hashOps.entries(KEY);
+    }
+
+    public void deletePerson(int id) {
+        hashOps.delete(KEY, id);
+    }
+}
